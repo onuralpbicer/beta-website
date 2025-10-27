e83f957d62c8708b9c790177dd9f808114497faa
Onur Alp Bicer <onuralpbicer@gmail.com>
2025-10-27 23:17:18 +1100

    chore: set up ssg with zoneless

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+  template: `<div>app</div>`,
+  styles: ``,
 })
-export class App {
-  protected title = 'beta-website';
-}
+export class App {}
