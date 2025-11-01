3f42f6521aead838f4c2cc3cdf76830196377b34
Onur Alp Bicer <onuralpbicer@gmail.com>
2025-11-01 08:41:54 +0000

    feat: Figured out more ssg routing

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,40 +1,14 @@
 import { PrerenderFallback, RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  { path: 'en-US', renderMode: RenderMode.Prerender },
   { path: 'en-US/about-us', renderMode: RenderMode.Prerender },
   { path: 'en-US/contact-us', renderMode: RenderMode.Prerender },
   { path: 'en-US/home', renderMode: RenderMode.Prerender },
-  { path: 'tr-TR', renderMode: RenderMode.Prerender },
   { path: 'tr-TR/hakkimizda', renderMode: RenderMode.Prerender },
   { path: 'tr-TR/home', renderMode: RenderMode.Prerender },
   { path: 'tr-TR/iletisim', renderMode: RenderMode.Prerender },
   {
     path: '**',
-    renderMode: RenderMode.Prerender,
-    fallback: PrerenderFallback.None,
-    getPrerenderParams: async () => {
-      return [];
-    },
-  },
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
-    fallback: PrerenderFallback.None,
-    getPrerenderParams: async () => {
-      return [];
-    },
-  },
-  {
-    path: 'en-US/**',
-    renderMode: RenderMode.Prerender,
-    fallback: PrerenderFallback.None,
-    getPrerenderParams: async () => {
-      return [];
-    },
-  },
-  {
-    path: 'tr-TR/**',
     renderMode: RenderMode.Prerender,
     fallback: PrerenderFallback.None,
     getPrerenderParams: async () => {
