e46b709fbee0eb4008127325891b14cdda0e508b
Onur Alp Bicer <onuralpbicer@gmail.com>
2025-12-22 13:20:40 +1100

    Set up sveltekit project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+    import './layout.css';
+    import favicon from '$lib/assets/favicon.svg';
+
+
+    let {children} = $props();
+</script>
+
+<svelte:head>
+    <link href={favicon} rel="icon"/>
+</svelte:head>
+{@render children()}
