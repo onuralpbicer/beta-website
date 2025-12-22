a4c9acc838af9a81b33bcb814206e9fac4bd7499
Onur Alp Bicer <onuralpbicer@gmail.com>
2025-12-22 21:29:22 +1100

    WIP better prerendering

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,53 @@
 import { redirect } from '@sveltejs/kit';
 import { getHomePageUrl } from '$lib/contentful.client';
+import { contentfulClient, getSluggableContentTypes } from '$lib/contentful.client';
+import type { EntryGenerator } from './$types';
 
 export const load = async () => {
 	redirect(308, await getHomePageUrl('tr'));
+};
+
+// idk why this doesn't work, but it's a better approach than what I already implemented
+export const entries: EntryGenerator = async () => {
+	const sluggableContentTypes = await getSluggableContentTypes();
+	const locales = await contentfulClient.getLocales();
+
+	const entries = [];
+
+	for (const contentType of sluggableContentTypes) {
+		const entriesList = await Promise.all(
+			locales.items.map(async (locale) => {
+				const entries = await contentfulClient.getEntries({
+					locale: locale.code,
+					content_type: contentType.sys.id,
+				});
+
+				return entries.items.map((entry) => ({
+					entry: entry,
+					locale: locale.code,
+				}));
+			}),
+		);
+
+		entries.push(entriesList.flat());
+	}
+
+	console.log([
+		...entries
+			.flat()
+			.filter((entry) => entry.entry.fields['slug'])
+			.map((entry) => `/${entry.locale}/${entry.entry.fields['slug']}`),
+		'/',
+		'/en',
+		'/tr',
+	]);
+	return [
+		...entries
+			.flat()
+			.filter((entry) => entry.entry.fields['slug'])
+			.map((entry) => `/${entry.locale}/${entry.entry.fields['slug']}`),
+		'/',
+		'/en',
+		'/tr',
+	];
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,42 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { createClient } from 'contentful';
-
-export const contentfulClient = createClient({
-	space: process.env['CONTENTFUL_SPACE'],
-	accessToken: process.env['CONTENTFUL_TOKEN'],
-	host: process.env['CONTENTFUL_HOST'],
-}).withoutLinkResolution;
-
-async function getSluggableContentTypes() {
-	const contentTypes = await contentfulClient.getContentTypes();
-	return contentTypes.items.filter((contentType) =>
-		contentType.fields.some((field) => field.id === 'slug'),
-	);
-}
-
-const sluggableContentTypes = await getSluggableContentTypes();
-const locales = await contentfulClient.getLocales();
-
-const entries = [];
-
-for (const contentType of sluggableContentTypes) {
-	const entriesList = await Promise.all(
-		locales.items.map(async (locale) => {
-			const entries = await contentfulClient.getEntries({
-				locale: locale.code,
-				content_type: contentType.sys.id,
-			});
-
-			return entries.items.map((entry) => ({
-				entry: entry,
-				locale: locale.code,
-			}));
-		}),
-	);
-
-	entries.push(entriesList.flat());
-}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -54,15 +17,6 @@ const config = {
 		}),
 		prerender: {
 			handleHttpError: 'warn',
-			entries: [
-				...entries
-					.flat()
-					.filter((entry) => entry.entry.fields['slug'])
-					.map((entry) => `/${entry.locale}/${entry.entry.fields['slug']}`),
-				'/',
-				'/en',
-				'/tr',
-			],
 		},
 	},
 };
