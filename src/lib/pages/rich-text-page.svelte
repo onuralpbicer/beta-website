6816bb9a3c796822263c9bad4e9b6be1b2564ca7
Onur Alp Bicer <onuralpbicer@gmail.com>
2025-12-24 11:00:29 +1100

    Render rich text pages

---
+++ src/lib/pages/rich-text-page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+    import type {IRichTextPageEntry} from "$lib/contentful";
+    import {documentToHtmlString} from '@contentful/rich-text-html-renderer';
+
+    let {richText}: { richText: IRichTextPageEntry } = $props();
+
+    const html = $derived(() => documentToHtmlString(richText.fields.content))
+</script>
+
+<section class="mx-auto w-full md:max-w-[70vw] md:min-w-175 px-8 my-8 min-h-[60dvh]">
+    <h1 class="heading-1 mb-8">{richText.fields.title}</h1>
+
+    {@html html()}
+</section>
--- src/routes/[locale]/[...slug]/+page.svelte
+++ src/routes/[locale]/[...slug]/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
     import type {PageProps} from './$types';
     import HomePage from '$lib/pages/home-page.svelte'
-    import type {IHomePageEntry} from "$lib/contentful";
+    import RichTextPage from '$lib/pages/rich-text-page.svelte'
+    import type {IHomePageEntry, IRichTextPageEntry} from "$lib/contentful";
 
     let {data, params}: PageProps = $props();
 
     const homePage = $derived(() => data.entry as IHomePageEntry)
+    const richTextPage = $derived(() => data.entry as IRichTextPageEntry)
 </script>
 
 {#if data.entry?.sys.contentType.sys.id === 'homePage'}
     <HomePage home={homePage()}/>
+{:else if (data.entry?.sys.contentType.sys.id === 'richTextPage') }
+    <RichTextPage richText={richTextPage()}/>
 {/if}
