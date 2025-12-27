2bc5585eccd7130e9df4ef7c8d100b39cd02c751
Onur Alp Bicer <onuralpbicer@gmail.com>
2025-12-27 13:43:18 +1100

    Present basic rich text

--- src/lib/pages/rich-text-page.svelte
+++ src/lib/pages/rich-text-page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 
     import type {IRichTextPage} from "$lib/sanity.model";
+    import {PortableText} from '@portabletext/svelte'
 
     let {richText}: { richText: IRichTextPage } = $props();
 
@@ -9,6 +10,5 @@
 <section class="mx-auto w-full md:max-w-[70vw] md:min-w-175 px-8 my-8 min-h-[60dvh]">
     <h1 class="heading-1 mb-8">{richText.title}</h1>
 
-    <!--{@html html()}-->
-    <div>{JSON.stringify(richText.page.content)}</div>
+    <PortableText value={richText.page.content ?? []} />
 </section>
