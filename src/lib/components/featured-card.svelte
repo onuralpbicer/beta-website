e008a33b4b332115192e98e3b7970777e3154586
Onur Alp Bicer <onuralpbicer@gmail.com>
2025-12-28 13:48:43 +1100

    Only do the hover animation for big screens

--- src/lib/components/featured-card.svelte
+++ src/lib/components/featured-card.svelte
@@ -4,7 +4,7 @@
     let {featured, locale}: { featured: FeaturedCard, locale: string } = $props();
 </script>
 
-<a class="shadow-md flex flex-col overflow-hidden rounded-md md:basis-1/3 hover:scale-110 transition-[scale]"
+<a class="shadow-md flex flex-col overflow-hidden rounded-md md:basis-1/3 md:hover:scale-110 transition-[scale]"
    href={`/${locale}/${featured.slug}`}>
     <img alt={featured.title} class="grow" src={featured.image}/>
     <div class="text-sm min-h-8 bg-gray-300 px-1 md:px-2 lg:px-4 font-bold flex flex-col justify-center">
