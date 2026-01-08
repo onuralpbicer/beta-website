c8d91360ac9a7c6343b217f5a20683a76e149956
Onur Alp Bicer <onuralpbicer@gmail.com>
2026-01-08 22:54:27 +1100

    Sanity document structure for products

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     internationalizedArray({
       languages,
       defaultLanguages: ['en', 'tr'],
-      fieldTypes: ['string', 'blockContent'],
+      fieldTypes: ['string', 'blockContent', 'file', 'number', 'boolean'],
     }),
     trTRLocale(),
   ],
--- studio/schemaTypes/product-page.ts
+++ studio/schemaTypes/product-page.ts
@@ -1,4 +1,4 @@
-import {defineType} from 'sanity'
+import {defineField, defineType} from 'sanity'
 import {pageFields, pagePreview, productOrCategoryFields} from './shared'
 
 export default defineType({
@@ -9,6 +9,85 @@ export default defineType({
   fields: [
     ...pageFields,
     ...productOrCategoryFields,
-    // todo later
+    defineField({
+      name: 'description',
+      type: 'internationalizedArrayString',
+      title: 'Tanımı',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'document',
+      type: 'internationalizedArrayFile',
+      title: 'Doküman',
+    }),
+    defineField({
+      name: 'brand',
+      type: 'internationalizedArrayString',
+      title: 'Marka',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'productCode',
+      type: 'internationalizedArrayString',
+      title: 'Ürün Kodu',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'price',
+      type: 'internationalizedArrayNumber',
+      title: 'Fiyatı',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'currency',
+      type: 'internationalizedArrayString',
+      title: 'Para Birimi',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'taxIncluded',
+      type: 'internationalizedArrayBoolean',
+      title: 'Vergi Dahil mi?',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'enKeyFeatures',
+      type: 'array',
+      title: 'İngilizce ürün açıklaması',
+      validation: (rule) => rule.required().min(1),
+      of: [
+        defineField({
+          name: 'item',
+          type: 'string',
+          title: 'Açıklama',
+          validation: (rule) => rule.required(),
+        }),
+      ],
+    }),
+    defineField({
+      name: 'trKeyFeatures',
+      type: 'array',
+      title: 'Türkçe ürün açıklaması',
+      validation: (rule) => rule.required().min(1),
+      of: [
+        defineField({
+          name: 'item',
+          type: 'string',
+          validation: (rule) => rule.required(),
+        }),
+      ],
+    }),
+    defineField({
+      name: 'relatedProducts',
+      type: 'array',
+      title: 'İlgili diğer ürünler',
+      of: [
+        defineField({
+          type: 'reference',
+          name: 'product',
+          to: [{type: 'productPage'}],
+        }),
+      ],
+    }),
   ],
 })
