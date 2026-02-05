c15d3dbd987eff4d1f18c0fdb44ffafb319b881a
Onur Alp Bicer <onuralpbicer@gmail.com>
2026-02-05 23:27:58 +1100

    Make robots not follow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,14 @@
     import './layout.css';
     import favicon from '$lib/assets/favicon.ico';
     import {dev} from '$app/environment';
-
+    import { PUBLIC_ENV } from '$env/static/public';
 
     let {children} = $props();
 </script>
 
 <svelte:head>
     <link href={favicon} rel="icon"/>
-    {#if (dev)}
+    {#if (dev || PUBLIC_ENV !== 'prod')}
         <meta name="robots" content="noindex, nofollow">
     {/if}
 </svelte:head>
