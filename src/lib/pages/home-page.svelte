7bfd2144a5ad5cf135a91bc95e89efcef97d5361
Onur Alp Bicer <onuralpbicer@gmail.com>
2026-07-25 12:39:33 +1000

    Remove why us and keywords, and adjust some styling

--- src/lib/pages/home-page.svelte
+++ src/lib/pages/home-page.svelte
@@ -3,7 +3,6 @@
     import {Button} from "$lib/components/ui/button/index.js";
     import type {IHomePage} from "$lib/sanity.model";
     import FeaturedCard from "$lib/components/featured-card.svelte";
-    import WhyUsCard from "$lib/components/why-us-card.svelte";
 
     let {home, locale}: { home: IHomePage, locale: string } = $props();
 
@@ -12,7 +11,7 @@
     const heroLink = $derived(() => home.page.linkTo!.slug)
 </script>
 <section
-        class="flex flex-col items-start justify-center relative h-[40dvh] md:h-[50dvh] after:absolute after:inset-0 after:block after:bg-primary after:opacity-50 after:z-[-1] text-primary-foreground">
+        class="flex flex-col items-start justify-center relative h-[40dvh] md:h-[40dvh] after:absolute after:inset-0 after:block after:bg-primary after:opacity-50 after:z-[-1] text-primary-foreground">
     <img alt="Hero" aria-hidden="true" class="z-[-1] absolute inset-0 h-full w-full object-cover"
          src={heroImageUrl()}/>
     <div class="mx-auto w-full md:max-w-[70vw] px-8">
@@ -26,24 +25,11 @@
         </a>
     </div>
 </section>
-<section>
-    <div class="mx-2 flex items-center justify-center gap-2 md:gap-4 mt-4 flex-wrap">
-        {#each home.page.keywords as keyword}
-            <span class="shadow-md border-gray-400 border px-2 rounded-md text-nowrap">{keyword}</span>
-        {/each}
-    </div>
-</section>
-<section class="mx-auto w-full md:max-w-[70vw] px-4 md:px-0 mb-4">
-    <h2 class="heading-2 my-4">{home.page.featuredTitle}</h2>
-    <div class="mx-2 md:mx-4 lg:mx-8 flex items-stretch justify-center gap-4 flex-wrap md:flex-nowrap">
+<section class="mx-auto w-full md:max-w-[75vw] px-4 md:px-0 mb-12">
+    <h2 class="heading-2 my-4 mb-8">{home.page.featuredTitle}</h2>
+    <div class="mx-2 md:mx-4 lg:mx-8 flex items-stretch justify-center gap-4 lg:gap-8 flex-wrap md:flex-nowrap">
         {#each home.page.featured as featured}
             <FeaturedCard featured={featured} locale={locale}/>
-        {/each}
-    </div>
-    <h2 class="heading-2 my-4">{home.page.whyUsTitle}</h2>
-    <div class="mx-2 md:mx-4 lg:mx-8 flex items-stretch justify-center gap-4 flex-wrap md:flex-nowrap">
-        {#each home.page.whyUs as whyUs}
-            <WhyUsCard whyUs={whyUs}/>
         {/each}
     </div>
 </section>
